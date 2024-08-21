76689958314d82ae674a284ba4095181bc4267c4
Randriamiandry <randriamiandryathanasios@gmail.com>
2024-08-21 19:07:29 +0300

    Update help.js

--- script/help.js
+++ script/help.js
@@ -33,7 +33,7 @@ module.exports.run = async function({
       eventCommands.forEach((eventCommand, index) => {
         helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
       });
-      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. Pour afficher la page suivante, tapez'${prefix}help numéro de page.'. To view information about a specific command, type '${prefix}help command name'.`;
+      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. Pour afficher la page suivante, tapez'${prefix}help numéro de page.'.To view information about a specific command, type '${prefix}help command name'.`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input);
